40061b85b26e33365b36578ba861fe235f0f50e7
Juancho Manzur <69224541+Pmorelius@users.noreply.github.com>
2023-04-07 20:52:25 -0500

    Se trabaja slider con informacion

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* menu toggle */
+
+var menuList = document.getElementById("menuList");
+
+menuList.style.maxHeight = "0px";
+
+function togglemenu(){
+    if(menuList.style.maxHeight == "0px")
+        {
+            menuList.style.maxHeight = "130px";
+        }
+        else
+        {
+            menuList.style.maxHeight = "0px";
+        }
+}
